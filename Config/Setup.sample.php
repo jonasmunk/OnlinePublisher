449557bd338e3885caa0ea20ab0a719f55c4c9f7
Jonas Munk <jonas@in2isoft.dk>
2010-07-01 21:12:34 +0200

    Added sample config file

---
+++ Config/Setup.sample.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package OnlinePublisher
+ * @subpackage Config
+ */
+
+$superUser="peter";
+$superPassword="$u9er$ecr3t";
+
+$database_host='localhost';
+$database_user='root';
+$database_password='';
+$database='onlinepublisher';
+
+$basePath='/path/on/disk/';
+$baseUrl='/~username/path/to/site/';
+
+$feedbackEmail = 'name@domain.com';
+
+$xwg_skin='In2ition';
+?>
