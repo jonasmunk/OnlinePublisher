f90dd606067556c88347992d18569eb34561f57b
Jonas Munk <jonas@in2isoft.dk>
2010-08-06 00:24:17 +0200

    Fixed fatal error on login page :-(

--- Editor/Services/Core/Authentication.php
+++ Editor/Services/Core/Authentication.php
@@ -4,13 +4,12 @@
  * @subpackage Services
  */
 require_once '../../../Config/Setup.php';
-require_once '../../Include/Functions.php';
-//require_once '../../Include/Security.php';
+require_once '../../Include/Public.php';
 require_once '../../Classes/Request.php';
 require_once '../../Classes/In2iGui.php';
 require_once '../../Classes/InternalSession.php';
 
-if (requestPost()) {
+if (Request::isPost()) {
 	$page = Request::getPostInt('page');
 	$username=Request::getPostString('username');
 	$password=Request::getPostString('password');
