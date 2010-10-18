f96194c8c5b633680360f399454754990a278f62
Jonas Munk <jonas@in2isoft.dk>
2010-10-19 00:39:59 +0200

    Converted image gallery part to use controller

--- Editor/Classes/Parts/ImagegalleryPartController.php
+++ Editor/Classes/Parts/ImagegalleryPartController.php
@@ -13,12 +13,53 @@ class ImagegalleryPartController extends PartController
 		parent::PartController('imagegallery');
 	}
 	
+	function getFromRequest($id) {
+		$part = ImagegalleryPart::load($id);
+		$part->setHeight(Request::getInt('height',64));
+		$part->setImageGroupId(Request::getInt('group'));
+		$part->setFramed(Request::getBoolean('framed'));
+		$part->setShowTitle(Request::getBoolean('showTitle'));
+		$part->setVariant(Request::getString('variant'));
+		return $part;
+	}
+	
 	function createPart() {
 		$part = new ImagegalleryPart();
 		$part->setHeight(100);
 		$part->setVariant('floating');
 		$part->save();
 		return $part;
+	}
+	
+	function display($part,$context) {
+		return $this->render($part,$context);
+	}
+	
+	function editor($part,$context) {
+		global $baseUrl;
+		return
+	    '<input type="hidden" name="group" value="'.$part->getImageGroupId().'"/>'.
+	    '<input type="hidden" name="height" value="'.$part->getHeight().'"/>'.
+	    '<input type="hidden" name="framed" value="'.StringUtils::toBoolean($part->getFramed()).'"/>'.
+	    '<input type="hidden" name="showTitle" value="'.StringUtils::toBoolean($part->getShowTitle()).'"/>'.
+	    '<input type="hidden" name="variant" value="'.$part->getVariant().'"/>'.
+		'<script src="'.$baseUrl.'Editor/Parts/imagegallery/script.js" type="text/javascript" charset="utf-8"></script>'.
+		'<div id="part_imagegallery_container">'.$this->render($part,$context).'</div>';
+	}
+		
+	function buildSub($part,$context) {
+		$data = '<imagegallery xmlns="'.$this->getNamespace().'">';
+		$data.='<display height="'.$part->getHeight().'" variant="'.StringUtils::escapeXML($part->getVariant()).'" framed="'.StringUtils::toBoolean($part->getFramed()).'" show-title="'.StringUtils::toBoolean($part->getShowTitle()).'"/>';
+		if ($part->getImageGroupId()) {
+			$sql="SELECT object.data from object,imagegroup_image where imagegroup_image.image_id = object.id and imagegroup_image.imagegroup_id=".Database::int($part->getImageGroupId())." order by object.title";
+			$result = Database::select($sql);
+			while ($row = Database::next($result)) {
+				$data.=$row['data'];
+			}
+			Database::free($result);
+		}
+		$data.='</imagegallery>';
+		return $data;
 	}
 	
 	function getToolbars() {
