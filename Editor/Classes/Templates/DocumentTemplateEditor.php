5c7c18c5250b6a5f925cf3a96cce5ee320c4a1fb
Jonas Munk <jonas@in2isoft.dk>
2010-10-22 11:00:51 +0200

    Document editor controller class

---
+++ Editor/Classes/Templates/DocumentTemplateEditor.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package OnlinePublisher
+ * @subpackage Classes.Templates
+ */
+require_once($basePath.'Editor/Classes/Templates/TemplateController.php');
+require_once($basePath.'Editor/Classes/Utilities/StringUtils.php');
+
+class DocumentTemplateEditor
+{
+		
+}
