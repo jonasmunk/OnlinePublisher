c71578b496fcf6bdd209ed4073a3f3f7150b9d40
Jonas Munk <jonas@in2isoft.dk>
2011-02-12 16:48:13 +0100

    Fixed missing class import

--- Editor/Classes/Parts/ListPartController.php
+++ Editor/Classes/Parts/ListPartController.php
@@ -10,6 +10,7 @@ require_once($basePath.'Editor/Classes/In2iGui.php');
 require_once($basePath.'Editor/Classes/Utilities/DateUtils.php');
 require_once($basePath.'Editor/Classes/Calendarsource.php');
 require_once($basePath.'Editor/Classes/News.php');
+require_once($basePath.'Editor/Classes/Objects/Newssource.php');
 require_once($basePath.'Editor/Classes/Model/Query.php');
 
 class ListPartController extends PartController
