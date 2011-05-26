90c799dcfb2f18d94573155c21a8d19083708499
Jonas Brinkmann Munk <jbm@atira.dk>
2011-05-26 14:58:14 +0200

    Made location picker load asynchronously

--- hui/js/LocationPicker.js
+++ hui/js/LocationPicker.js
@@ -7,7 +7,8 @@ hui.ui.LocationPicker = function(options) {
 	this.name = options.name;
 	this.options = options.options || {};
 	this.element = hui.get(options.element);
-	this.defaultCenter = new google.maps.LatLng(57.0465, 9.9185);
+	this.backendLoaded = false;
+	this.defered = [];
 	hui.ui.extend(this);
 }
 
@@ -22,44 +23,68 @@ hui.ui.LocationPicker.prototype = {
 			button.listen({$click:function() {panel.hide()}});
 			panel.add(buttons.add(button));
 			hui.setStyle(panel.element,{left:'-10000px',top:'-10000px',display:''});
-		    var mapOptions = {
-		      zoom: 15,
-		      mapTypeId: google.maps.MapTypeId.TERRAIN
-		    }
-		    this.map = new google.maps.Map(mapContainer, mapOptions);
-			google.maps.event.addListener(this.map, 'click', function(obj) {
-				var loc = {latitude:obj.latLng.lat(),longitude:obj.latLng.lng()};
-    			this.setLocation(loc);
-				this.fire('locationChanged',loc);
-  			}.bind(this));
-			panel.element.style.display = 'none';
+			this._whenReady(function() {
+		   	 	var mapOptions = {
+			      zoom: 15,
+			      mapTypeId: google.maps.MapTypeId.TERRAIN
+			    }
+				this.defaultCenter = new google.maps.LatLng(57.0465, 9.9185);
+			    this.map = new google.maps.Map(mapContainer, mapOptions);
+				google.maps.event.addListener(this.map, 'click', function(obj) {
+					var loc = {latitude:obj.latLng.lat(),longitude:obj.latLng.lng()};
+	    			this.setLocation(loc);
+					this.fire('locationChanged',loc);
+	  			}.bind(this));
+				this.setLocation(options.location);
+			}.bind(this))
 		}
-		this.setLocation(options.location);
 		if (options.node) {
 			this.panel.position(options.node);
 		}
 		this.panel.show();
 	},
-	setLocation : function(loc) {
-		if (!loc && this.marker) {
-			this.marker.setMap(null);
-			this.map.setCenter(this.defaultCenter);
+	_whenReady : function(func) {
+		if (this.backendLoaded) {
+			func();
 			return;
 		}
-		loc = this.buildLatLng(loc);
-		if (!this.marker) {
-		    this.marker = new google.maps.Marker({
-		        position: loc, 
-		        map: this.map
-		    });
-		} else {
-    		this.marker.setPosition(loc);
-			this.marker.setMap(this.map);
-		}
-		this.map.setCenter(loc);
+		this.defered.push(func);
+		if (this.loadingBackend) {return};
+		this.loadingBackend = true;
+		window.huiLocationPickerReady = function() {
+			this.loadingBackend = false;
+			this.backendLoaded = true;
+			hui.log('Google maps loaded!')
+			for (var i=0; i < this.defered.length; i++) {
+				this.defered[i]();
+			};
+			window.huiLocationPickerReady = null;
+		}.bind(this);
+		hui.log('Loading google maps...')
+		hui.require('http://maps.google.com/maps/api/js?sensor=false&callback=huiLocationPickerReady');
 	},
-	/** @private */
-	buildLatLng : function(loc) {
+	setLocation : function(loc) {
+		this._whenReady(function() {
+			hui.log('Setting location...')
+			if (!loc && this.marker) {
+				this.marker.setMap(null);
+				this.map.setCenter(this.defaultCenter);
+				return;
+			}
+			loc = this._buildLatLng(loc);
+			if (!this.marker) {
+			    this.marker = new google.maps.Marker({
+			        position: loc, 
+			        map: this.map
+			    });
+			} else {
+	    		this.marker.setPosition(loc);
+				this.marker.setMap(this.map);
+			}
+			this.map.setCenter(loc);
+		}.bind(this))
+	},
+	_buildLatLng : function(loc) {
 		if (!loc) {
 			loc = {latitude:57.0465, longitude:9.9185};
 		}
