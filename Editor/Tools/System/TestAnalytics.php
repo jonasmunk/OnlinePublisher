8baee26ad0ff6a104243de055838b967fb4ae5d2
Jonas Munk <jonas@in2isoft.dk>
2011-06-28 18:38:04 +0200

    Fixed problem with testing analytics integration

--- Editor/Tools/System/TestAnalytics.php
+++ Editor/Tools/System/TestAnalytics.php
@@ -5,7 +5,7 @@
  */
 require_once '../../../Config/Setup.php';
 require_once '../../Include/Security.php';
-require_once '../../Classes/Intergration/GoogleAnalytics.php';
+require_once '../../Classes/Integration/GoogleAnalytics.php';
 require_once '../../Classes/In2iGui.php';
 
 $result = GoogleAnalytics::test();
