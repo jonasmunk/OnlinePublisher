d5408829d965567b032ec7126c7413c11ab96948
Jonas Munk <jonas@in2isoft.dk>
2011-08-26 14:51:51 +0200

    Improved Info service and Central tool

--- services/info/json/index.php
+++ services/info/json/index.php
@@ -4,7 +4,9 @@ require_once '../../../Editor/Include/Public.php';
 require_once('../../../Editor/Classes/SystemInfo.php');
 require_once('../../../Editor/Classes/Response.php');
 require_once('../../../Editor/Classes/Services/TemplateService.php');
-require_once('../../../Editor/Classes/Tool.php');
+require_once('../../../Editor/Classes/Services/MailService.php');
+require_once('../../../Editor/Classes/Services/ToolService.php');
+require_once('../../../Editor/Classes/Utilities/StringUtils.php');
 
 Response::sendObject(array(
 	'date' => SystemInfo::getDate(),
@@ -12,6 +14,18 @@ Response::sendObject(array(
 		'installed' => TemplateService::getInstalledTemplateKeys(),
 		'used' => TemplateService::getUsedTemplates()
 	),
-	'tools' => Tool::getInstalledToolKeys()
+	'tools' => array(
+		'installed' => ToolService::getInstalledToolKeys()
+	),
+	'email' => array(
+		'enabled' => MailService::getEnabled(),
+		'server' => StringUtils::isNotBlank(MailService::getServer()),
+		'username' => StringUtils::isNotBlank(MailService::getUsername()),
+		'password' => StringUtils::isNotBlank(MailService::getPassword()),
+		'standardEmail' => StringUtils::isNotBlank(MailService::getStandardEmail()),
+		'standardName' => StringUtils::isNotBlank(MailService::getStandardName()),
+		'feedbackEmail' => StringUtils::isNotBlank(MailService::getFeedbackEmail()),
+		'feedbackName' => StringUtils::isNotBlank(MailService::getFeedbackName())
+	)
 ));
 ?>
