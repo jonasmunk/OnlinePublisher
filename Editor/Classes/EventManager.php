a6c8934031474ee86cdebb55b8b30d8a476dc3d5
Jonas Munk <jonas@in2isoft.dk>
2011-09-13 18:57:05 +0200

    Centralised model event handling

--- Editor/Classes/EventManager.php
+++ Editor/Classes/EventManager.php
@@ -18,8 +18,35 @@ class EventManager {
 	 */
 	function fireEvent($event,$type,$subType,$id) {
 		global $basePath;
-		require($basePath.'Editor/Template/document/Events.php');
-		require($basePath.'Editor/Template/sitemap/Events.php');
+		if (($event=='publish' || $event=='delete') && $type=='object' && $subType=='image') {
+			$sql = "select distinct page_id from document_section,part_image where document_section.part_id=part_image.part_id and part_image.image_id=".$id;
+			$result = Database::select($sql);
+			while ($row=Database::next($result)) {
+				$sql = "update page set changed=now() where id=".$row['page_id'];
+				Database::update($sql);
+			}
+			Database::free($result);
+		} elseif (($event=='publish' || $event=='delete') && $type=='object' && $subType=='person') {
+			$sql = "select distinct page_id from document_section,part_person where document_section.part_id=part_person.part_id and part_person.person_id=".$id;
+			$result = Database::select($sql);
+			while ($row=Database::next($result)) {
+				$sql = "update page set changed=now() where id=".$row['page_id'];
+				Database::update($sql);
+			}
+			Database::free($result);
+		} elseif (($event=='publish' || $event=='delete' || $event=='update') && $type=='object' && $subType=='file') {
+			$sql = "select distinct page_id from document_section,part_file where document_section.part_id=part_file.part_id and part_file.file_id=".$id;
+			$result = Database::select($sql);
+			while ($row=Database::next($result)) {
+				$sql = "update page set changed=now() where id=".$row['page_id'];
+				Database::update($sql);
+			}
+			Database::free($result);
+		}
+		if ($type=='hierarchy') {
+			$sql = "update page,template set page.changed=now() where page.template_id=template.id and template.unique='sitemap'";
+			Database::update($sql);
+		}
 		if ($type=='object' && $subType=='image' && ($event=='publish' || $event=='delete')) {
 			// Update persons and news with images	
 		    $sql = "update object,person set object.updated=now() where object.id=person.object_id and image_id=".$id;
