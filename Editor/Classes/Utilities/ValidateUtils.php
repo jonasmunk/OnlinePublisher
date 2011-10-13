79c202b2045f293b90af04b66d4ffd66fd015cfb
Jonas Munk <jonas@in2isoft.dk>
2011-10-13 19:39:48 +0200

    Improved URL validation

--- Editor/Classes/Utilities/ValidateUtils.php
+++ Editor/Classes/Utilities/ValidateUtils.php
@@ -13,11 +13,18 @@ class ValidateUtils {
 	function validateEmail($email) {
 		return preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})/i",$email)>0;
 	}
+
+	function validateHref($url) {
+		if ($url[0]=='#') {
+			return true;
+		}
+		return ValidateUtils::validateUrl($url);
+	}
 	
 	function validateUrl($url) {
-		$pattern = "/\Ahttp:\/\/[a-z0-9\-\.]+\.[a-z0-9]{2,3}\/[a-z0-9.\?&\/\#=_\-\)\(;]*\z/i";
-	   $num = preg_match($pattern,$url);
-	   return ($num>0);
+		$pattern = "/\Ahttp[s]?:\/\/[a-z0-9\-\.]+\.[a-z0-9]{2,3}[\/]?[a-z0-9.\?&\/\#=_\-\%)\(;,+]*\z/i";
+		$num = preg_match($pattern,$url);
+		return ($num>0);
 	}
 	
 	function validateDigits($str=null) {
