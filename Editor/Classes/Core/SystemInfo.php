bd48104477df7197abc20a45e84d87353e2ec160
Jonas Munk <jonas@in2isoft.dk>
2011-11-03 22:21:33 +0100

    New version

--- Editor/Classes/Core/SystemInfo.php
+++ Editor/Classes/Core/SystemInfo.php
@@ -6,8 +6,8 @@ if (!isset($GLOBALS['basePath'])) {
 require_once($basePath.'Editor/Classes/Utilities/DateUtils.php');
 class SystemInfo {
 	
-	private static $date = 30;
-	private static $month = 10;
+	private static $date = 3;
+	private static $month = 11;
 	private static $year = 2011;
 	private static $feedbackMail = "jonasmunk@mac.com";
 	private static $feedbackName = "Jonas Munk";
