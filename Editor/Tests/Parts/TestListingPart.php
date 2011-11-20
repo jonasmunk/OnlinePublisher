1c83e6039132462d3176bf90bc1bb0e984e144ba
Jonas Munk <jonas@in2isoft.dk>
2011-11-20 11:10:48 +0100

    Improved part tests

--- Editor/Tests/Parts/TestListingPart.php
+++ Editor/Tests/Parts/TestListingPart.php
@@ -38,6 +38,31 @@ class TestListingPart extends UnitTestCase {
 		
 		$obj2->remove();
 	}
+	
+	function testBuild() {
+		$obj = new ListingPart();
+		$obj->setId(20);
+		$obj->setText("* Lorem [s]ipsum[s] dolor [e]sit[e] amet,\n consectetur<tag> [slet]adipisicing[slet] elit\n\nNew paragraph\n\n\nThree & new lines");
+		$obj->setColor('#eee');
+		$obj->setFontFamily('Verdana');
+		$ctrl = new ListingPartController();
+		$xml = $ctrl->build($obj,new PartContext());
+		$expected = '<part xmlns="http://uri.in2isoft.com/onlinepublisher/part/1.0/" type="listing" id="20">'.
+			'<sub>'.
+			'<listing xmlns="http://uri.in2isoft.com/onlinepublisher/part/listing/1.0/">'.
+			'<style color="#eee" font-family="Verdana"/>'.
+			'<list type="">'.
+			'<item>'.
+				'<first> Lorem <strong>ipsum</strong> dolor <em>sit</em> amet,</first>'.
+				'<break/>'.
+				' consectetur&lt;tag&gt; <del>adipisicing</del> elit<break/><break/>New paragraph<break/><break/><break/>Three &amp; new lines'.
+			'</item>'.
+			'</list>'.
+			'</listing>'.
+			'</sub>'.
+			'</part>';
+		$this->assertEqual($xml,$expected);
+	}
 
 	function testImport() {
 		$obj = new ListingPart();
