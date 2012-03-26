1b3e7897401cee577ac997b819dad835afce24e0
Jonas Munk <jonasmunk@mac.com>
2012-03-26 21:25:04 +0200

    Added color wheel and initial support for better part menu

--- Editor/Classes/Services/PartService.php
+++ Editor/Classes/Services/PartService.php
@@ -116,23 +116,46 @@ union select html as text,document_section.part_id from part_table,document_sect
 
 	function getParts() {
 		return array(
-			'header' => array ( 'name' => array('da'=>'Overskrift','en'=>'Header'), 'description' => '', 'priority' => 0 ),
-			'text' => array ( 'name' => array('da'=>'Tekst','en'=>'Text'), 'description' => '', 'priority' => 1 ),
-			'listing' => array ( 'name' => array('da'=>'Punktopstilling','en'=>'Bullet list'), 'description' => '', 'priority' => 2 ),
-			'image' => array ( 'name' => array('da'=>'Billede','en'=>'Image'), 'description' => '', 'priority' => 3 ),
-			'imagegallery' => array ( 'name' => array('da'=>'Billedgalleri','en'=>'Image gallery'), 'description' => '', 'priority' => 8 ),
-			'horizontalrule' => array ( 'name' => array('da'=>'Adskiller','en'=>'Divider'), 'description' => '', 'priority' => 4 ),
-			'table' => array ( 'name' => array('da'=>'Tabel','en'=>'Table'), 'description' => '', 'priority' => 4 ),
-			'richtext' => array ( 'name' => array('da'=>'Rig tekst','en'=>'Rich text'), 'description' => '', 'priority' => 8 ),
-			'file' => array ( 'name' => array('da'=>'Fil','en'=>'File'), 'description' => '', 'priority' => 7 ),
-			'person' => array ( 'name' => array('da'=>'Person','en'=>'Person'), 'description' => '', 'priority' => 5 ),
-			'news' => array ( 'name' => array('da'=>'Nyheder','en'=>'News'), 'description' => '', 'priority' => 6 ),
-			'formula' => array ( 'name' => array('da'=>'Formular','en'=>'Formula'), 'description' => '', 'priority' => 10 ),
-			'list' => array ( 'name' => array('da'=>'Liste','en'=>'List'), 'description' => '', 'priority' => 4 ),
-			'mailinglist' => array ( 'name' => array('da'=>'Postliste','en'=>'Mailing list'), 'description' => '', 'priority' => 10 ),
-			'html' => array ( 'name' => array('da'=>'HTML','en'=>'HTML'), 'description' => '', 'priority' => 7 ),
-			'poster' => array ( 'name' => array('da'=>'Plakat','en'=>'Poster'), 'description' => '', 'priority' => 7 )
+			'header' => array ( 'name' => array('da'=>'Overskrift','en'=>'Header') ),
+			'text' => array ( 'name' => array('da'=>'Tekst','en'=>'Text') ),
+			'listing' => array ( 'name' => array('da'=>'Punktopstilling','en'=>'Bullet list') ),
+			'image' => array ( 'name' => array('da'=>'Billede','en'=>'Image') ),
+			'imagegallery' => array ( 'name' => array('da'=>'Billedgalleri','en'=>'Image gallery') ),
+			'horizontalrule' => array ( 'name' => array('da'=>'Adskiller','en'=>'Divider') ),
+			'table' => array ( 'name' => array('da'=>'Tabel','en'=>'Table') ),
+			'richtext' => array ( 'name' => array('da'=>'Rig tekst','en'=>'Rich text') ),
+			'file' => array ( 'name' => array('da'=>'Fil','en'=>'File') ),
+			'person' => array ( 'name' => array('da'=>'Person','en'=>'Person') ),
+			'news' => array ( 'name' => array('da'=>'Nyheder','en'=>'News') ),
+			'formula' => array ( 'name' => array('da'=>'Formular','en'=>'Formula') ),
+			'list' => array ( 'name' => array('da'=>'Liste','en'=>'List') ),
+			'mailinglist' => array ( 'name' => array('da'=>'Postliste','en'=>'Mailing list') ),
+			'html' => array ( 'name' => array('da'=>'HTML','en'=>'HTML') ),
+			'poster' => array ( 'name' => array('da'=>'Plakat','en'=>'Poster') )
 		);
+	}
+	
+	function getPartMenu() {
+		$parts = PartService::getParts();
+		$menu = array(
+			'header' => $parts['header'],
+			'text' => $parts['text'],
+			'listing' => $parts['listing'],
+			'image' => $parts['image'],
+			'imagegallery' => $parts['imagegallery'],
+			'horizontalrule' => $parts['horizontalrule'],
+			'table' => $parts['table'],
+			'richtext' => $parts['richtext'],
+			'file' => $parts['file'],
+			'person' => $parts['person'],
+			'news' => $parts['news'],
+			'formula' => $parts['formula'],
+			'list' => $parts['list'],
+			'mailinglist' => $parts['mailinglist'],
+			'html' => $parts['html'],
+			'poster' => $parts['poster']
+		);
+		return $menu;
 	}
 	
 	function getAllControllers() {
