97ff47922f9dc6c9d19a9fba11010a309f8f7fc4
Jonas Munk <jonas@in2isoft.dk>
2012-07-18 22:26:00 +0200

    Fixed a problem with column margins + a number of minor style issues on in2isoft.dk

--- style/basic/js/OnlinePublisher.js
+++ style/basic/js/OnlinePublisher.js
@@ -425,7 +425,7 @@ op.part.Poster.prototype = {
 	_onClick : function(e) {
 		e = hui.event(e);
 		var a = e.findByTag('a');
-		if (a) {
+		if (a && hui.cls.has(a.parentNode,'part_poster_navigator')) {
 			this.goToPage(parseInt(a.getAttribute('data')));
 		}
 	}
