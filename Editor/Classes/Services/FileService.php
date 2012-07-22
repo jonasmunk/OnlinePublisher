3724e8c8779bd05c2e657778c05308a3f0e3d3f6
Jonas Munk <jonas@in2isoft.dk>
2012-07-22 14:55:25 +0200

    Move some SQL to services

--- Editor/Classes/Services/FileService.php
+++ Editor/Classes/Services/FileService.php
@@ -325,10 +325,25 @@ class FileService {
 	}
 	
 	function getFileFilename($id) {
-		$sql = "select filename from file where object_id=".$id;
+		$sql = "select filename from file where object_id=".Database::int($id);
 		if ($row = Database::selectFirst($sql)) {
 			return $row['filename'];
 		}
 		return NULL;
+	}
+	
+	function getGroupCounts() {
+		$out = array();
+		$sql="select distinct object.id,object.title,count(file.object_id) as filecount from filegroup, filegroup_file, file,object  where filegroup_file.filegroup_id=filegroup.object_id and filegroup_file.file_id = file.object_id and object.id=filegroup.object_id group by filegroup.object_id union select object.id,object.title,'0' from object left join filegroup_file on filegroup_file.filegroup_id=object.id where object.type='filegroup' and filegroup_file.file_id is null order by title";
+		$result = Database::select($sql);
+		while ($row = Database::next($result)) {
+			$out[] = array(
+				'id' => $row['id'],
+				'title' => $row['title'],
+				'count' => $row['filecount']
+			);
+		}
+		Database::free($result);
+		return $out;
 	}
 }
--- Editor/Services/Finder/FilesSelection.php
+++ Editor/Services/Finder/FilesSelection.php
@@ -18,21 +18,20 @@ $writer->item(array(
 
 $writer->title('Grupper');
 
-$sql="select distinct object.id,object.title,count(file.object_id) as filecount from filegroup, filegroup_file, file,object  where filegroup_file.filegroup_id=filegroup.object_id and filegroup_file.file_id = file.object_id and object.id=filegroup.object_id group by filegroup.object_id union select object.id,object.title,'0' from object left join filegroup_file on filegroup_file.filegroup_id=object.id where object.type='filegroup' and filegroup_file.file_id is null order by title";
-$result = Database::select($sql);
-while ($row = Database::next($result)) {
+$groups = FileService::getGroupCounts();
+
+foreach ($groups as $group) {
 	$options = array(
-		'value' => $row['id'],
-		'title' => $row['title'],
+		'value' => $group['id'],
+		'title' => $group['title'],
 		'icon' => 'common/folder',
 		'kind' => 'filegroup'
 	);
-	if ($row['filecount']>0) {
-		$options['badge']=$row['filecount'];
+	if ($group['filecount']>0) {
+		$options['badge']=$group['filecount'];
 	}
-	$writer->startItem($options)->endItem();
+	$writer->startItem($options)->endItem();	
 }
-Database::free($result);
 
 $writer->endItems();
 ?>
