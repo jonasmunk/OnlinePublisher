a01ad7f795ab0ae7457092438bf3becc28029b07
Jonas Munk <jonas@in2isoft.dk>
2012-08-06 21:38:32 +0200

    Minor cleanup

--- Editor/Services/Core/Authentication.php
+++ Editor/Services/Core/Authentication.php
@@ -6,7 +6,7 @@
 require_once '../../../Config/Setup.php';
 require_once '../../Include/Public.php';
 require_once '../../Classes/Core/Request.php';
-require_once '../../Classes/Interface/In2iGui.php';
+require_once '../../Classes/Core/Response.php';
 require_once '../../Classes/Core/InternalSession.php';
 require_once '../../Classes/Services/ToolService.php';
 require_once '../../Classes/Objects/User.php';
@@ -19,16 +19,16 @@ if (Request::isPost()) {
 	
 	if (InternalSession::logIn($username,$password)) {
 		ToolService::install('System'); // Ensure that the system tool is present
-		In2iGui::sendObject(array('success' => true));
+		Response::sendUnicodeObject(array('success' => true));
 	} else {
 		usleep(rand(1500000,3000000)); // Wait for random amount of time
-		In2iGui::sendObject(array('success' => false));
+		Response::sendUnicodeObject(array('success' => false));
 	}
 	
 } else {
 	
 	usleep(rand(1500000,3000000));  // Wait for random amount of time
-	In2iGui::sendObject(array('success' => false));
+	Response::sendUnicodeObject(array('success' => false));
 
 }
 exit;
