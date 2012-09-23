0764df3e61ee0122708289a6b6a2f0f48379199a
Jonas Munk <jonasmunk@mac.com>
2012-09-23 11:53:58 +0200

    Fixed HFS design issues

--- Editor/Classes/Core/SystemInfo.php
+++ Editor/Classes/Core/SystemInfo.php
@@ -6,7 +6,7 @@ if (!isset($GLOBALS['basePath'])) {
 
 class SystemInfo {
 	
-	private static $date = 22;
+	private static $date = 23;
 	private static $month = 9;
 	private static $year = 2012;
 	private static $feedbackMail = "jonasmunk@mac.com";
