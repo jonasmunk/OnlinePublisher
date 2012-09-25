cd7b320b62587aac28992a50ae732187795cf483
Jonas Munk <jonas@in2isoft.dk>
2012-09-25 14:11:10 +0200

    Fixed problem with moving hierarchy item

--- Editor/Tools/Sites/actions/RelocateHierarchyItem.php
+++ Editor/Tools/Sites/actions/RelocateHierarchyItem.php
@@ -9,7 +9,12 @@ $move = Request::getInt('move',0);
 $targetItem = Request::getInt('targetItem',0);
 $targetHierarchy = Request::getInt('targetHierarchy',0);
 
+
+Log::debug($targetItem);
+
 $response = Hierarchy::relocateItem($move,$targetItem,$targetHierarchy);
+
+Log::debug($response);
 
 Response::sendObject($response);
 ?>
