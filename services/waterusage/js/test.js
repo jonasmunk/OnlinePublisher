2a4464b305d9db9b8cef6f8c8dc2333baacd5ac8
Jonas Munk <jonasmunk@mac.com>
2013-01-02 22:04:11 +0100

    Improved test page for water usage registration

--- services/waterusage/js/test.js
+++ services/waterusage/js/test.js
@@ -1,32 +1,138 @@
 hui.ui.listen({
+	
+	number : null,
+	data : null,
+	
 	$ready : function() {
 		this.numberInput = new hui.ui.Input({element:'number'});
 		this.numberInput.listen({
 			$valueChanged : function(value) {
-				this._fetch(value);
+				//this._fetch(value);
 			}.bind(this)
 		})
 		
-		this.numberInput.setValue('26108502');
-		this._fetch('20116254');
+		this.numberInput.setValue('20116254');
+		
+		this.dateInput = new hui.ui.Input({element:'date',validator:this._validateDate});
+		this.dateInput.setValue(new Date().dateFormat('d-m-Y'));
+		this.valueInput = new hui.ui.Input({element:'value',validator:this._validateNumber});
+		this.submit = new hui.ui.Input({element:'submit',name:'submit'});
+		
+		hui.listen(hui.get('edit'),'click',this._editInfo.bind(this));
+		
+		new hui.ui.Input({element:'sendNumber',name:'sendNumber'})
+
+		this.$click$sendNumber();
 	},
+	
+	
+	$click$submit : function() {
+		var data = {
+			number : this.number,
+			date : this.dateInput.value,
+			value : this.valueInput.value
+		}
+		var valid = true;
+		if (hui.isBlank(data.date)) {
+			this.dateInput.setError('Skal udfyldes');
+			this.dateInput.focus()
+			valid = false;
+		} else {
+			this.dateInput.setError();
+		}
+		if (hui.isBlank(data.value)) {
+			this.valueInput.setError('Skal udfyldes');
+			if (valid) {
+				this.valueInput.focus()
+			}
+			valid = false;
+		} else {
+			this.valueInput.setError();
+		}
+		if (!valid) {
+			return;
+		}
+		hui.ui.showMessage({text:'Gemmer værdi...',busy:true})
+		hui.ui.request({
+			url : 'register.php',
+			parameters : data,
+			$success : function() {
+				hui.ui.showMessage({text:'Den nye værdi er gemt',icon:'common/success',duration:2000})
+				this._fetch(this.number);
+			}.bind(this),
+			$failure : function() {
+				hui.ui.showMessage({text:'Det lykkedes desværre ikke at gemme værdien',icon:'common/warning',duration:3000})
+			}
+		})
+	},
+	
+	$click$sendNumber : function() {
+		this._fetch(this.numberInput.getValue());
+	},
+	
 	_fetch : function(number) {
+		this.number = number;
 		hui.ui.request({
 			url : 'load.php',
 			parameters : {number:number},
 			$object : function(obj) {
+				this.data = obj;
 				this._renderRecord(obj.usage);
 				this._renderChart(obj.usage);
 				this._renderInfo(obj.info);
+				this.valueInput.focus();
 			}.bind(this),
 			$failure : function() {
-				alert('Something terrible happened')
+				hui.ui.showMessage({text:'Det lykkedes desværre ikke at hente data',icon:'common/warning',duration:3000})
 			},
 			$exception : function(e) {
 				throw e;
 			}
 		});		
 	},
+	
+	_editInfo : function() {
+		if (!this.infoBox) {
+			var box = this.infoBox = hui.ui.Box.create({title:'Kontaktinformation',absolute:true,width:300,padding:10,closable:true,modal:true});
+			var form = this.infoForm = hui.ui.Formula.create({name:'infoForm'});
+			var group = form.buildGroup({above:true},[
+				{type:'TextField',label:'E-mail-adresse',options:{key:'email'}},
+				{type:'TextField',label:'Telefon',options:{key:'phone'}}
+			])
+			var buttons = group.createButtons();
+			buttons.add(hui.ui.Button.create({text:'Annuller',name:'cancelInfo'}));
+			buttons.add(hui.ui.Button.create({text:'Gem',highlighted:true,submit:true}));
+			box.add(form)
+			box.addToDocument();
+		}
+		this.infoForm.setValues(this.data.info);
+		this.infoBox.show();
+		this.infoForm.focus();
+	},
+	$click$cancelInfo : function() {
+		this.infoBox.hide();
+	},
+	$submit$infoForm : function(form) {
+		var values = form.getValues(),
+			box = this.infoBox;
+		values.number = this.number;
+		hui.ui.request({
+			url : 'update_info.php',
+			parameters : values,
+			message : {start:'Gemmer information...',success:'De nye kontaktoplysninger er gemt'},
+			$success : function() {
+				box.hide();
+				this._fetch(this.number);
+			}.bind(this),
+			$failure : function() {
+				hui.ui.showMessage({text:'Det lykkedes desværre ikke at gemme informationen',icon:'common/warning',duration:3000});
+				box.hide();
+			}
+		})
+	},
+	
+	////// Rendering //////
+	
 	_renderChart : function(obj) {
 		hui.get('chart').innerHTML = '';
 		var chart = new hui.ui.Chart.create({parent:'chart'});
@@ -50,6 +156,7 @@ hui.ui.listen({
 			var row = hui.build('tr',{parent:body});
 			hui.build('th',{parent:row,text:data[i].date});
 			hui.build('td',{parent:row,text:data[i].value});
+			hui.build('td',{parent:row,text:data[i].perweek});
 		};
 	},
 	_renderInfo : function(info) {
@@ -71,5 +178,41 @@ hui.ui.listen({
 		var loc = 'http://maps.googleapis.com/maps/api/staticmap?center='+address.replace(/[\W]+/g,'%20')+'&zoom=16&size=640x300&sensor=false';
 		map.style.backgroundImage = 'url("'+loc+'")';
 		//hui.build('img',{src:loc,parent:map})
+	},
+	
+	
+	
+	
+	////// Validators //////
+	
+	_validateDate : {
+		_dateFormats : ['d-m-Y','d/m-Y','d/m/Y','d-m-Y H:i:s','d/m-Y H:i:s','d/m/Y H:i:s','d-m-Y H:i','d/m-Y H:i','d/m/Y H:i','d-m-Y H','d/m-Y H','d/m/Y H','d-m','d/m','d','Y','m-d-Y','m-d','m/d'],
+		validate : function(value) {
+			for (var i=0; i < this._dateFormats.length; i++) {
+				var fmt = this._dateFormats[i];
+				var parsed = Date.parseDate(value,fmt);
+				
+				if (parsed) {
+					return {valid:true,value:parsed.dateFormat('d-m-Y')};
+				}
+			};
+			return {valid:false,value:''};
+		}
+	},
+	_validateNumber : {
+		validate : function(value) {
+			var valid = false;
+			if (value) {
+				var pattern = /[0-9]+/g;
+				var matches = value.match(pattern);
+				if (matches) {
+					value = matches.join('');
+				} else {
+					value = '';
+					valid = false;
+				}			
+			}
+			return {valid:true,value:value}
+		}
 	}
 })
--- services/waterusage/load.php
+++ services/waterusage/load.php
@@ -7,6 +7,20 @@ $sql = "select DATE_FORMAT(waterusage.date, '%d-%m-%Y') as `date`,`value`,UNIX_T
 	" from waterusage,watermeter where waterusage.`watermeter_id`=watermeter.`object_id` and number = ".Database::text($number)." order by waterusage.`date`";
 $rows = Database::selectAll($sql);
 
+$latest = 0;
+$previousValue;
+foreach ($rows as &$row) {
+	if ($latest==0) {
+		$row['perweek'] = '?';
+	} else {
+		$time = intval($row['time']) - $latest;
+		$amount = intval($row['value']) - $previousValue;
+		$row['perweek'] = number_format($amount / ($time / 60 / 60 / 24) * 7,4);
+	}
+	$latest = intval($row['time']);
+	$previousValue = intval($row['value']);
+}
+
 $summary = WaterusageService::getSummary($number);
 
 Response::sendObject(array(
