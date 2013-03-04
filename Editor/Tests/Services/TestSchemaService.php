034bc3d65d5a504476a915fb126bffe68eefec8a
Jonas Munk <jonasmunk@mac.com>
2013-03-04 21:57:25 +0100

    Fixed tests

--- Editor/Tests/Services/TestSchemaService.php
+++ Editor/Tests/Services/TestSchemaService.php
@@ -15,7 +15,7 @@ class TestSchemaService extends UnitTestCase {
 		$obj = new ImagegalleryPart();
 	
 		$sql = SchemaService::buildSqlColumns(Part::$schema['imagegallery']);
-		$this->assertEqual($sql,'`variant`,`height`,`imagegroup_id`,`framed`,`show_title`');
+		$this->assertEqual($sql,'`variant`,`height`,`width`,`imagegroup_id`,`framed`,`show_title`');
 		
 		$obj->setVariant('block');
 		$obj->setHeight(100);
@@ -24,11 +24,11 @@ class TestSchemaService extends UnitTestCase {
 		$obj->setShowTitle(true);
 		
 		$sql = SchemaService::buildSqlValues($obj,Part::$schema['imagegallery']);
-		$this->assertEqual($sql,"'block',100,78,0,1");
+		$this->assertEqual($sql,"'block',100,0,78,0,1");
 		
 		
 		$sql = SchemaService::buildSqlSetters($obj,Part::$schema['imagegallery']);
-		$this->assertEqual($sql,"`variant`='block',`height`=100,`imagegroup_id`=78,`framed`=0,`show_title`=1");
+		$this->assertEqual($sql,"`variant`='block',`height`=100,`width`=0,`imagegroup_id`=78,`framed`=0,`show_title`=1");
     }
 }
 ?>
