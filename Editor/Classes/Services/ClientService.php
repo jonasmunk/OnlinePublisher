c1897baae5bfca7a8d13f06cf86ceafbfdda57d7
Jonas Munk <jonas@in2isoft.dk>
2013-04-06 13:49:25 +0200

    Added client service

---
+++ Editor/Classes/Services/ClientService.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package OnlinePublisher
+ * @subpackage Classes.Services
+ */
+if (!isset($GLOBALS['basePath'])) {
+	header('HTTP/1.1 403 Forbidden');
+	exit;
+}
+
+class ClientService {
+
+
+	function getStatistics() {
+		$query = new StatisticsQuery();
+		$query->setStartTime(DateUtils::addDays(time(),-7));
+		$stats = StatisticsService::searchVisits($query);
+
+		$result = array();
+
+		foreach ($stats as $stat) {
+			$obj = new stdClass;
+			$obj->date = $stat['key'];
+			$obj->hits = intval($stat['hits']);
+			$obj->ips = intval($stat['ips']);
+			$obj->sessions = $stat['sessions'];
+			$result[] = $obj;
+		}
+		return $result;
+	}
+}
