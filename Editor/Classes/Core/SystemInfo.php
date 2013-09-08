a103a80d6f7b58701ab6d37b0c4d38761e779102
Jonas Munk <jonasmunk@mac.com>
2013-09-08 22:01:23 +0200

    Restored part change event

--- Editor/Classes/Core/SystemInfo.php
+++ Editor/Classes/Core/SystemInfo.php
@@ -6,7 +6,7 @@ if (!isset($GLOBALS['basePath'])) {
 
 class SystemInfo {
 	
-	private static $date = 6;
+	private static $date = 8;
 	private static $month = 9;
 	private static $year = 2013;
 	private static $feedbackMail = "jonasmunk@mac.com";
