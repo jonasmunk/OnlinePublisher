7c9df77b2570866927071cb9d6c41a77a7a510ee
Jonas Munk <jm@peytz.dk>
2013-12-18 22:11:30 +0100

    Fixed feedback url

--- Editor/Services/Start/data/SendFeedback.php
+++ Editor/Services/Start/data/SendFeedback.php
@@ -9,7 +9,7 @@ $message = Request::getString('message');
 
 $user = User::load(InternalSession::getUserId());
 
-$request = new WebRequest('http://www.in2isoft.dk/services/issues/create/');
+$request = new WebRequest('http://www.humanise.dk/services/issues/create/');
 $request->setParameters(array('description'=>$message,'site'=>ConfigurationService::getBaseUrl(),'user'=>$user->getUsername()));
 
 $client = new HttpClient();
