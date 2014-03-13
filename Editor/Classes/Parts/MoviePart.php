224f9f877d57f6b13b090b1a7662de04ccf252cf
Jonas Munk <jm@peytz.dk>
2014-03-13 08:29:49 +0100

    More fields on movie part

--- Editor/Classes/Parts/MoviePart.php
+++ Editor/Classes/Parts/MoviePart.php
@@ -11,14 +11,20 @@ if (!isset($GLOBALS['basePath'])) {
 Part::$schema['movie'] = array(
 	'fields' => array(
 		'fileId' => array( 'type' => 'int', 'column' => 'file_id' ),
-		'text' => array( 'type' => 'text' )
+		'imageId' => array( 'type' => 'int', 'column' => 'image_id' ),
+		'text' => array( 'type' => 'text' ),
+		'url' => array( 'type' => 'text' ),
+		'code' => array( 'type' => 'text' )
 	)
 );
 
 class MoviePart extends Part
 {
 	var $fileId;
+	var $imageId;
 	var $text;
+	var $url;
+	var $code;
 	
 	function MoviePart() {
 		parent::Part('movie');
@@ -36,12 +42,36 @@ class MoviePart extends Part
 	    return $this->fileId;
 	}
 	
+	function setImageId($imageId) {
+	    $this->imageId = $imageId;
+	}
+
+	function getImageId() {
+	    return $this->imageId;
+	}
+	
 	function setText($text) {
 	    $this->text = $text;
 	}
 
 	function getText() {
 	    return $this->text;
+	}
+	
+	function setUrl($url) {
+	    $this->url = $url;
+	}
+
+	function getUrl() {
+	    return $this->url;
+	}
+	
+	function setCode($code) {
+	    $this->code = $code;
+	}
+
+	function getCode() {
+	    return $this->code;
 	}
 }
 ?>
