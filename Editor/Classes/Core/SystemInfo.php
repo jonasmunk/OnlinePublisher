30c838998cd7ab1cb680d08555963575c5e9561a
Jonas Munk <jonasmunk@mac.com>
2014-03-25 21:22:05 +0100

    New version

--- Editor/Classes/Core/SystemInfo.php
+++ Editor/Classes/Core/SystemInfo.php
@@ -6,7 +6,7 @@ if (!isset($GLOBALS['basePath'])) {
 
 class SystemInfo {
 	
-	private static $date = 6;
+	private static $date = 24;
 	private static $month = 3;
 	private static $year = 2014;
 	private static $feedbackMail = "jonasmunk@mac.com";
