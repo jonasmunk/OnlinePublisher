5ee1bd4290a6ec7249919e25cb53570a801f8b26
Jonas Munk <jm@peytz.dk>
2014-08-23 00:32:51 +0200

    Updated Grunt config

--- hui/grunt/Gruntfile.js
+++ hui/grunt/Gruntfile.js
@@ -3,12 +3,6 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            build: {
-                src: '../js/Alert.js',
-                dest: '../tmp/Alert.min.js'
-            }
-        },
         jshint: {
             all: ['../js/*.js']
         },
@@ -19,7 +13,6 @@ module.exports = function(grunt) {
     });
 
     // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     
