8e2f4d8a74e8b333735aeaebae7c992a9e973a0a
Jonas Munk <jm@peytz.dk>
2014-10-14 14:56:14 +0200

    Removed unused code

--- Editor/Classes/Parts/MenuPartController.php
+++ Editor/Classes/Parts/MenuPartController.php
@@ -13,7 +13,7 @@ class MenuPartController extends PartController
 	function MenuPartController() {
 		parent::PartController('menu');
 	}
-	
+
 	static function createPart() {
 		$part = new MenuPart();
 		$part->setHierarchyId(HierarchyService::getLatestId());
@@ -21,11 +21,11 @@ class MenuPartController extends PartController
 		$part->save();
 		return $part;
 	}
-	
+
 	function display($part,$context) {
 		return $this->render($part,$context);
 	}
-	
+
 	function editor($part,$context) {
 		return '<div id="part_menu_container">'.$this->render($part,$context).'</div>'.
 
@@ -36,7 +36,7 @@ class MenuPartController extends PartController
         )).
 		'<script src="'.ConfigurationService::getBaseUrl().'Editor/Parts/menu/editor.js" type="text/javascript" charset="utf-8"></script>';
 	}
-	
+
 	function getFromRequest($id) {
 		$part = MenuPart::load($id);
 		$part->setHierarchyId(Request::getInt('hierarchyId'));
@@ -44,7 +44,7 @@ class MenuPartController extends PartController
 		$part->setDepth(Request::getInt('depth'));
 		return $part;
 	}
-	
+
 	function buildSub($part,$context) {
 		$xml='<menu xmlns="'.$this->getNamespace().'"' .
             ' hierarchy-id="' . Strings::escapeXML($part->getHierarchyId()) . '"' .
@@ -59,24 +59,23 @@ class MenuPartController extends PartController
             if ($item) {
                 $xml.= '<items>';
                 $xml.=HierarchyService::hierarchyTraveller($item->getHierarchyId(),$item->getId(),false,$depth);
-                $xml.= '</items>';                
+                $xml.= '</items>';
             }
         }
 
 		$xml.='</menu>';
 		return $xml;
 	}
-    	
+
 	function importSub($node,$part) {
         $menu = DOMUtils::getFirstChildElement($node,'menu');
         if ($menu) {
             $part->setHierarchyId(intval($menu->getAttribute('hierarchy-id')));
-            $part->setVariant($menu->getAttribute('variant'));            
+            $part->setVariant($menu->getAttribute('variant'));
             $part->setDepth(intval($menu->getAttribute('depth')));
         }
 	}
-	
-	
+
 	function getToolbars() {
 		return array(
 			GuiUtils::getTranslated(array('Menu','da'=>'Menu')) =>
@@ -96,25 +95,6 @@ class MenuPartController extends PartController
 			</field>
 		'
 		);
-	}
-	
-	
-	
-	function editorGui($part,$context) {
-		$gui='
-		<window title="{Add file; da:Tilføj fil}" name="fileUploadWindow" width="300" padding="10">
-			<upload name="fileUpload" url="../../Parts/file/Upload.php" widget="upload">
-				<placeholder 
-					title="{Select a file on your computer; da:Vælg en fil på din computer...}" 
-					text="{The file size can at most be; da:Filens størrelse må højest være} '.GuiUtils::bytesToString(FileSystemService::getMaxUploadSize()).'."/>
-			</upload>
-			<buttons align="center" top="10">
-				<button name="cancelUpload" title="{Close; da:Luk}"/>
-				<button name="upload" title="{Select file...; da:Vælg fil...}" highlighted="true"/>
-			</buttons>
-		</window>
-		';
-		return UI::renderFragment($gui);
 	}
 }
 ?>
