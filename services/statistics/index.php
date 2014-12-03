d46da423ee59290bb3ca4b35737e240c76b961c0
Jonas Munk <jm@peytz.dk>
2014-12-03 08:46:28 +0100

    Re-enabled cookies

--- services/statistics/index.php
+++ services/statistics/index.php
@@ -1,6 +1,9 @@
 <?php
 require_once '../../Editor/Include/Public.php';
 
+session_set_cookie_params(0);
+session_start();
+
 StatisticsService::registerPage([
 	'id' => Request::getInt('page'),
 	'referrer' => Request::getString('referrer'),
