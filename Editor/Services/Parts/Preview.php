ccff07fdeaa8b7f5c075cee38995146123ef6db5
Jonas Munk <jonasmunk@mac.com>
2015-04-02 13:34:58 +0200

    Use correct context when previewing widget

--- Editor/Services/Parts/Preview.php
+++ Editor/Services/Parts/Preview.php
@@ -12,8 +12,7 @@ $pageId = Request::getInt('pageId');
 if ($controller = PartService::getController($type)) {
 	if ($part = $controller->getFromRequest($id)) {
 		header("Content-Type: text/html; charset=UTF-8");
-		$context = new PartContext();
-		$context->setLanguage(PageService::getLanguage($pageId));
+		$context = DocumentTemplateController::buildPartContext($pageId);
 		echo $controller->render($part,$context);
 	}
 }
