7820edadba5124a14f4597ab547816cf329f76e4
Jonas Munk <jm@peytz.dk>
2015-08-15 10:36:56 +0200

    Fixed problem with starting session

--- Editor/Services/Core/Authentication.php
+++ Editor/Services/Core/Authentication.php
@@ -6,18 +6,15 @@
 require_once '../../Include/Public.php';
 
 if (Request::isPost()) {
-    session_set_cookie_params(0);
-    session_start();
-	
-	$page = Request::getInt('page');
-	$username = Request::getString('username');
-	$password = Request::getString('password');
-	
-	if (InternalSession::logIn($username,$password)) {
-		ToolService::install('System'); // Ensure that the system tool is present
-		Response::sendObject(array('success' => true));
-        exit;
-	}
+  $page = Request::getInt('page');
+  $username = Request::getString('username');
+  $password = Request::getString('password');
+  
+  if (InternalSession::logIn($username,$password)) {
+    ToolService::install('System'); // Ensure that the system tool is present
+    Response::sendObject(array('success' => true));
+    exit;
+  }
 }
 usleep(rand(1500000,3000000));  // Wait for random amount of time
 Response::sendObject(array('success' => false));
