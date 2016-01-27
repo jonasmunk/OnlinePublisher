79c4c9cbfcef8ea4740d14850a78f83262261534
Jonas Munk <jonasmunk@mac.com>
2016-01-27 20:43:55 +0100

    Recompiled

--- hui/bin/all.js
+++ hui/bin/all.js
@@ -9822,17 +9822,17 @@ hui.ui.ImageViewer.create = function(options) {
 hui.ui.ImageViewer.prototype = {
 
 	nodes : {
-		viewer : 'hui_imageviewer_viewer',
-		innerViewer : 'hui_imageviewer_inner_viewer',
+		viewer : '.hui_imageviewer_viewer',
+		innerViewer : '.hui_imageviewer_inner_viewer',
 
-		status : 'hui_imageviewer_status',
-		text : 'hui_imageviewer_text',
+		status : '.hui_imageviewer_status',
+		text : '.hui_imageviewer_text',
 
-		previous : 'hui_imageviewer_previous',
-		controller : 'hui_imageviewer_controller',
-		next : 'hui_imageviewer_next',
-		play : 'hui_imageviewer_play',
-		close : 'hui_imageviewer_close'
+		previous : '.hui_imageviewer_previous',
+		controller : '.hui_imageviewer_controller',
+		next : '.hui_imageviewer_next',
+		play : '.hui_imageviewer_play',
+		close : '.hui_imageviewer_close'
 	},
 
 	_attach : function() {
@@ -12661,8 +12661,8 @@ hui.ui.Box.create = function(options) {
 
 hui.ui.Box.prototype = {
 	nodes : {
-    	body : 'hui_box_body',
-    	close : 'hui_box_close'
+    	body : '.hui_box_body',
+    	close : '.hui_box_close'
 	},
 	_close : function(e) {
 		hui.stop(e);
