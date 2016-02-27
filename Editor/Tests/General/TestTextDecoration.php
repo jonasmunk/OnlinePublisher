79557ba9be060cbb90afa328253c94a0bc085709
Jonas Munk <jm@peytz.dk>
2016-02-27 11:40:22 +0100

    Support tilde in links

--- Editor/Tests/General/TestTextDecoration.php
+++ Editor/Tests/General/TestTextDecoration.php
@@ -19,22 +19,22 @@ class TestTextDecoration extends UnitTestCase {
 		return $dec;		
 	}
 
-    function testMarkup() {
-        $text = 'Lorem [s]ipsum[s] dolor sdjahsdak@dshajsak.com sit amet, http://www.joansmunk.dk/ consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam [s]ipsumfafdasdfaa[s] sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra posuere sapien. Nam consectetuer. Sed aliquam, nunc eget euismod ullamcorper, lectus nunc ullamcorper orci, fermentum bibendum enim nibh eget ipsum. Donec porttitor ligula eu dolor. http://www.apple.com/ Maecenas vitae nulla jbm@ah.dk consequat libero cursus venenatis. Nam magna enim, accumsan eu, blandit sed, blandit a, eros.';
+  function testMarkup() {
+    $text = 'Lorem [s]ipsum[s] dolor sdjahsdak@dshajsak.com sit amet, http://www.joansmunk.dk/ consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam [s]ipsumfafdasdfaa[s] sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra posuere sapien. Nam consectetuer. Sed aliquam, nunc eget euismod ullamcorper, lectus nunc ullamcorper orci, fermentum bibendum enim nibh eget ipsum. Donec porttitor ligula eu dolor. http://www.apple.com/ Maecenas vitae nulla jbm@ah.dk consequat libero cursus venenatis. Nam magna enim, accumsan eu, blandit sed, blandit a, eros.';
 		$expected = '<strike>Lor</strike>em <strong>ipsum</strong> dolor <a href="mailto:sdjahsdak@dshajsak.com">sdjahsdak@dshajsak.com</a> sit amet, <a href="http://www.joansmunk.dk/">http://www.joansmunk.dk/</a> consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam <strong>ipsumfafdasdfaa</strong> sit amet enim. Suspendisse id velit vitae ligula volutpat condimentum. Aliquam erat volutpat. Sed quis velit. Nulla facilisi. Nulla libero. Vivamus pharetra posuere sapien. Nam consectetuer. Sed aliquam, nunc eget euismod ullamcorper, lectus nunc ullamcorper orci, fermentum bibendum enim nibh eget ipsum. Donec porttitor ligula eu dolor. <a href="http://www.apple.com/">http://www.apple.com/</a> Maecenas vitae nulla <a href="mailto:jbm@ah.dk">jbm@ah.dk</a> consequat libero cursus venenatis. Nam magna enim, accumsan eu, blandit sed, blandit a, eros.';
 		$result = $this->getDecorator()->decorate($text);
 		$this->assertEqual($expected,$result);
-    }
+  }
     
-    function testMarkup2() {
+  function testMarkup2() {
 		// Test that tags do not span multiple lines
-        $text = "First line\n[s]Second line\n\nThird line[s]\nFourth line";
+    $text = "First line\n[s]Second line\n\nThird line[s]\nFourth line";
 		$expected = "First line\n[s]Second line\n\nThird line[s]\nFourth line";
 		$result = $this->getDecorator()->decorate($text);
 		$this->assertEqual($expected,$result);
-    }
+  }
 
-    function testUrl() {
+  function testUrl() {
 		$text = 'Lorem ipsum dolor sit amet, http://www.apple.com/ consectetur adipisicing elit';
 		$expected = 'Lorem ipsum dolor sit amet, <a href="http://www.apple.com/">http://www.apple.com/</a> consectetur adipisicing elit';
 		$this->doUrlTest($text,$expected);
@@ -50,6 +50,11 @@ class TestTextDecoration extends UnitTestCase {
 		$text = 'lorem http://msdn.microsoft.com ipsum';
 		$expected = 'lorem <a href="http://msdn.microsoft.com">http://msdn.microsoft.com</a> ipsum';
 		$this->doUrlTest($text,$expected);
+
+		$text = 'lorem http://ai.stanford.edu/~rion/swn/ ipsum';
+		$expected = 'lorem <a href="http://ai.stanford.edu/~rion/swn/">http://ai.stanford.edu/~rion/swn/</a> ipsum';
+		$this->doUrlTest($text,$expected);    
+    
 	}
 	
 	function doUrlTest($text,$expected) {
