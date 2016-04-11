88000c07cd4fa3ff0e13b37e5a84930a25b917ba
Jonas Munk <jonasmunk@mac.com>
2016-04-11 10:27:09 +0200

    Class on header level change

--- Editor/Parts/header/toolbar.js
+++ Editor/Parts/header/toolbar.js
@@ -37,11 +37,11 @@ hui.ui.listen({
 	},
 	$valueChanged$level : function(value) {
 		this.form.level.value=value || '';
-		this.form.text.className="common_font part_header part_header_"+value;
+		this.form.text.className="common_font part_header part_header-"+value;
 		for (var i=0;i<=6;i++) {
-			hui.cls.remove(partToolbar.section,'part_section_header_'+i);
+			hui.cls.remove(partToolbar.section,'part_section_header-'+i);
 		}
-		hui.cls.add(partToolbar.section,'part_section_header_'+value);
+		hui.cls.add(partToolbar.section,'part_section_header-'+value);
 		partToolbar.getMainController().syncSize();
 	},
 	$valueChanged$fontWeight : function(value) {
