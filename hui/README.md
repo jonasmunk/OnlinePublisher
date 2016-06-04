d5397f99129e9d2f95cef0a00f457b861c10c9cf
Jonas Munk <jonasmunk@mac.com>
2016-06-04 11:18:39 +0200

    Removed HUI from repo - use its own repo from now on

--- hui/README.md
+++
@@ -1,4 +0,0 @@
-# hui
-Humanise User Interface
-
-A library for web user interfaces
--- hui/test/html/upload.php
+++
@@ -1,7 +0,0 @@
-<?php
-usleep(rand(0,6000000));
-error_log('Number of files: '.count($_FILES));
-error_log(json_encode($_FILES));
-
-echo 'SUCCESS';
-?>
