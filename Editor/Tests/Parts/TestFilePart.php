953ceec223693c00ea74e2013524cd644a49e20c
Jonas Munk <jonasmunk@mac.com>
2016-07-17 23:22:04 +0200

    Fixed indentation

--- Editor/Tests/Parts/TestFilePart.php
+++ Editor/Tests/Parts/TestFilePart.php
@@ -5,59 +5,59 @@
  */
 
 if (!isset($GLOBALS['basePath'])) {
-	header('HTTP/1.1 403 Forbidden');
-	exit;
+  header('HTTP/1.1 403 Forbidden');
+  exit;
 }
 
 class TestFilePart extends UnitTestCase {
-    
-    function testLoad() {
-        $this->assertNull(FilePart::load(0));
+
+  function testLoad() {
+    $this->assertNull(FilePart::load(0));
+  }
+
+  function testCreate() {
+    $obj = new FilePart();
+    $this->assertFalse($obj->isPersistent());
+    $obj->save();
+    $this->assertTrue($obj->isPersistent());
+    $id = $obj->getId();
+    $this->assertNotNull(FilePart::load($id));
+    $obj->remove();
+    $this->assertNull(FilePart::load($id));
+  }
+
+  function testProperties() {
+    $obj = new FilePart();
+    $obj->setFileId(10);
+    $obj->save();
+
+    $obj2 = FilePart::load($obj->getId());
+    $this->assertEqual($obj2->getFileId(),10);
+
+    $obj2->remove();
+  }
+
+  function testImport() {
+    $obj = new FilePart();
+    $latest = FileService::getLatestFileId();
+    if ($latest==null) {
+      Log::debug('This test can only run with at least one file present');
+      return;
     }
+    $obj->setFileId($latest);
+    $obj->setText('Get me back!');
+    $ctrl = new FilePartController();
 
-    function testCreate() {
-        $obj = new FilePart();
-		$this->assertFalse($obj->isPersistent());
-		$obj->save();
-		$this->assertTrue($obj->isPersistent());
-		$id = $obj->getId();
-        $this->assertNotNull(FilePart::load($id));
-		$obj->remove();
-        $this->assertNull(FilePart::load($id));
-    }
+    $xml = $ctrl->build($obj,new PartContext());
 
-	function testProperties() {
-		$obj = new FilePart();
-		$obj->setFileId(10);
-		$obj->save();
-		
-		$obj2 = FilePart::load($obj->getId());
-		$this->assertEqual($obj2->getFileId(),10);
-		
-		$obj2->remove();
-	}
+    $this->assertNull($ctrl->importFromString(null));
 
-	function testImport() {
-		$obj = new FilePart();
-		$latest = FileService::getLatestFileId();
-		if ($latest==null) {
-			Log::debug('This test can only run with at least one file present');
-			return;
-		}
-		$obj->setFileId($latest);
-		$obj->setText('Get me back!');
-		$ctrl = new FilePartController();
-		
-		$xml = $ctrl->build($obj,new PartContext());
-		
-		$this->assertNull($ctrl->importFromString(null));
-		
-		$imported = $ctrl->importFromString($xml);
-		
-		$this->assertNotNull($imported);
-		$this->assertIdentical($imported->getFileId(),$obj->getFileId());
-		$this->assertIdentical($imported->getText(),$obj->getText());
-	}
-        
+    $imported = $ctrl->importFromString($xml);
+
+    $this->assertNotNull($imported);
+    $this->assertIdentical($imported->getFileId(),$obj->getFileId());
+    $this->assertIdentical($imported->getText(),$obj->getText());
+  }
+
 }
 ?>
