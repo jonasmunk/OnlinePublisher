eafd672bf242ac38f450188f19ef688223e3cf89
Jonas Munk <jonasmunk@mac.com>
2016-07-20 12:12:30 +0200

    Configuration on populate stream stage

--- Editor/Classes/Modules/Workflows/PopulateStreamStage.php
+++ Editor/Classes/Modules/Workflows/PopulateStreamStage.php
@@ -12,10 +12,16 @@ class PopulateStreamStage extends WorkflowStage {
 
   private $streamId;
   private $itemPath;
+  private $timeProperty;
+  private $identityProperty;
+  private $clear;
 
   function PopulateStreamStage(array $options = []) {
     $this->streamId = isset($options['id']) ? $options['id'] : null;
     $this->itemPath = isset($options['itemPath']) ? $options['itemPath'] : null;
+    $this->timeProperty = isset($options['timeProperty']) ? $options['timeProperty'] : null;
+    $this->identityProperty = isset($options['identityProperty']) ? $options['identityProperty'] : null;
+    $this->clear = isset($options['clear']) ? $options['clear'] == 'true' : false;
   }
 
   function run($state) {
@@ -25,23 +31,38 @@ class PopulateStreamStage extends WorkflowStage {
       $state->fail();
       return;
     }
+    if ($this->clear) {
+      $existing = Query::after('streamitem')
+              ->withProperty(Streamitem::$STREAM_ID, $stream->getId())
+              ->get();
+      foreach ($existing as $item) {
+        $item->remove();
+      }
+    }
     $now = time();
     $obj = $state->getData();
-    $items = $obj->getItems(); // TODO
+    $items = WorkflowService::evaluate($obj, $this->itemPath);
     foreach ($items as $item) {
       $data = Strings::toJSON($item);
-      $hash = md5($data);
-      $existing = Query::after('streamitem')
-        ->withProperty(Streamitem::$HASH,$hash)
-          ->withProperty(Streamitem::$STREAM_ID,$stream->getId())->first();
-      if ($existing) {
-        Log::debug('Skipping stream item: ' . $hash);
-        continue;
+      $identity = WorkflowService::evaluate($item, $this->identityProperty);
+      if (!$identity) {
+        $identity = $hash;
       }
-      $streamItem = new Streamitem();
+      $hash = md5($data);
+      $streamItem = Query::after('streamitem')
+        ->withProperty(Streamitem::$IDENTITY, $identity)
+        ->withProperty(Streamitem::$STREAM_ID, $stream->getId())
+        ->first();
+      if (!$streamItem) {
+        $streamItem = new Streamitem();
+      }
       $streamItem->setStreamId($stream->getId());
       $streamItem->setData($data);
-      $streamItem->setOriginalDate($item->getPubDate());
+      $time = intval(WorkflowService::evaluate($item, $this->timeProperty));
+      if ($time > 0) {
+        $streamItem->setOriginalDate($time);
+      }
+      $streamItem->setIdentity($identity);
       $streamItem->setRetrievalDate($now);
       $streamItem->setHash($hash);
       $streamItem->save();
