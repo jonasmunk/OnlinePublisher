f01cef674a8fc719e509febc63f093c76b587bfe
Jonas Munk <jonasmunk@mac.com>
2016-07-31 14:34:27 +0200

    Fixed JS dependency in exhibit #janemunk

--- style/janemunk/js/exhibit.js
+++ style/janemunk/js/exhibit.js
@@ -1,12 +1,12 @@
-(function(_) {
+require(['hui'], function(hui) {
 
   var exhibit = {
     space : 'concrete',
 
     init : function() {
       var self = this;
-      _.on(_.find('.js-spaces'),'tap',function(e) {
-        e = _.event(e);
+      hui.on(hui.find('.js-spaces'),'tap',function(e) {
+        e = hui.event(e);
         var option = e.findByClass('js-spaces-option');
         if (option) {
           e.stop();
@@ -14,19 +14,19 @@
           hui.cls.remove(document.body,'exhibit-' + self.space);
           hui.cls.add(document.body,'exhibit-' + nw);
           self.space = nw;
-          var x = _.findAll('.js-spaces-option');
+          var x = hui.findAll('.js-spaces-option');
           for (var i = 0; i < x.length; i++) {
-            _.cls.remove(x[i],'is-selected');
+            hui.cls.remove(x[i],'is-selected');
           }
-          _.cls.add(option,'is-selected');
+          hui.cls.add(option,'is-selected');
         }
       })
     }
   }
 
   var viewer = {
-    element : _.find('.js-viewer'),
-    inner : _.find('.js-viewer-inner'),
+    element : hui.find('.js-viewer'),
+    inner : hui.find('.js-viewer-inner'),
     source : null,
     busy : false,
     zoomed : false,
@@ -71,7 +71,7 @@
 
       mc.on("rotatestart panstart", function(ev) {
         if (self.busy) {return;}
-        //_.log('-- ' + ev.type + ': rotate=' + ev.rotation + ', scale=' + ev.scale + ', move=' + ev.deltaX +'x'+ev.deltaY+', num=' + ev.srcEvent.targetTouches.length);
+        //hui.log('-- ' + ev.type + ': rotate=' + ev.rotation + ', scale=' + ev.scale + ', move=' + ev.deltaX +'x'+ev.deltaY+', num=' + ev.srcEvent.targetTouches.length);
         start.rotation = ev.type=='rotatestart' ? ev.rotation : undefined;
         start.scale = ev.type=='rotatestart' ? ev.scale : undefined;
         start.x = start.x || ev.deltaX;
@@ -84,7 +84,7 @@
 
       mc.on("pinch rotate pan", function(ev) {
         if (self.busy) {return;}
-        //_.log(ev.type + ': rotate=' + ev.rotation + ', scale=' + ev.scale + ', move=' + ev.deltaX +'x'+ev.deltaY+', num=' + ev.srcEvent.targetTouches.length);
+        //hui.log(ev.type + ': rotate=' + ev.rotation + ', scale=' + ev.scale + ', move=' + ev.deltaX +'x'+ev.deltaY+', num=' + ev.srcEvent.targetTouches.length);
         if (start.scale==undefined) {start.scale = ev.scale}
         if (ev.srcEvent.targetTouches.length == 2) {
           if (start.rotation==undefined) {start.rotation = ev.rotation}
@@ -93,10 +93,10 @@
         session.scale = 1 + ev.scale - start.scale;
         session.x = ev.deltaX - start.x;
         session.y = ev.deltaY - start.y;
-        //_.log(ev);
+        //hui.log(ev);
         var rotation = (session.rotation + self.state.rotation) % 360;
         /*if (ev.type=='pan') {
-          _.log(rotation);
+          hui.log(rotation);
         }*/
         if (rotation > 180) {
           rotation -= 360;
@@ -104,8 +104,8 @@
           rotation += 360;
         }
         var scale = session.scale * self.state.scale;
-        //_.log((curRotation + self.state.rotation) + ' / ' + rotation);
-        //_.log(session.x + 'x' + session.y);
+        //hui.log((curRotation + self.state.rotation) + ' / ' + rotation);
+        //hui.log(session.x + 'x' + session.y);
         if (!true) {
           self.image.style.transform = 'translate3d(' + (session.x + self.state.x) + 'px,' + (session.y + self.state.y) + 'px,0) rotate(' + rotation + 'deg)';
           self.image.style.width = (500*scale) + 'px';
@@ -129,7 +129,7 @@
       });
       mc.on("rotateend panend", function(ev) {
         if (self.busy) {return;}
-        //_.log('-- ' + ev.type + ': rotate=' + ev.rotation + ', scale=' + ev.scale + ', move=' + ev.deltaX +'x'+ev.deltaY+', num=' + ev.srcEvent.targetTouches.length);
+        //hui.log('-- ' + ev.type + ': rotate=' + ev.rotation + ', scale=' + ev.scale + ', move=' + ev.deltaX +'x'+ev.deltaY+', num=' + ev.srcEvent.targetTouches.length);
         self.state.scale *= session.scale;
         self.state.x += session.x;
         self.state.y += session.y;
@@ -147,8 +147,8 @@
           y : 0
         };
         start.rotation = undefined;
-        //_.log(self.state);
-        //_.log(self.state.scale);
+        //hui.log(self.state);
+        //hui.log(self.state.scale);
         if (self.state.scale<.4) {
           self.hide();
         }
@@ -164,14 +164,14 @@
     hide : function() {
       if (this.busy) {return;}
       this.busy = true;
-      //_.log('hide')
+      //hui.log('hide')
       var lmnt = this.element,
         inner = this.inner;
       lmnt.style.transition = '';
       this._reset();
       this._setViewerSize();
-      _.cls.add(lmnt,'is-small');
-      _.cls.remove(lmnt,'is-full');
+      hui.cls.add(lmnt,'is-small');
+      hui.cls.remove(lmnt,'is-full');
       inner.style.transition = 'all .5s';
       inner.style.transform = '';
       lmnt.style.transition = 'all .5s';
@@ -185,7 +185,7 @@
         this.source.style.opacity = '';
         window.setTimeout(function() {
 
-          _.cls.remove(lmnt,'is-small');
+          hui.cls.remove(lmnt,'is-small');
           inner.style.transition = '';
           lmnt.style.transition = '';
           self.busy = false;
@@ -203,11 +203,11 @@
 
       var src = this.source;
       var element = this.element;
-      var pos = _.position.get(src);
+      var pos = hui.position.get(src);
       pos.left -= src.parentNode.parentNode.scrollLeft;
-      var w = _.window.getViewWidth();
-      var h = _.window.getViewHeight();
-      _.style.set(element,{
+      var w = hui.window.getViewWidth();
+      var h = hui.window.getViewHeight();
+      hui.style.set(element,{
         transition: 'all .5s',
         top: pos.top + 'px',
         left: pos.left + 'px',
@@ -227,19 +227,19 @@
     },
 
     updateImage : function(options) {
-      _.cls.add(options.node,'is-loading-image');
+      hui.cls.add(options.node,'is-loading-image');
       var width = options.width;
       width = Math.ceil(width / 100) * 100;
       var height = options.height;
       height = Math.ceil(height / 100) * 100;
       var url = this.getUrl(options.id,width,height);
-      _.log(url);
+      hui.log(url);
       var img = new Image();
       img.onload = function() {
-        _.style.set(options.node,{
+        hui.style.set(options.node,{
           backgroundImage : 'url(' + url + ')'
         });
-        _.cls.remove(options.node,'is-loading-image');
+        hui.cls.remove(options.node,'is-loading-image');
       }
       img.src = url;
     },
@@ -250,8 +250,8 @@
       this.busy = true;
       this._reset();
       var src = this.source = options.src;
-      var bg = _.style.get(src,'background-image');
-      _.style.set(this.inner,{
+      var bg = hui.style.get(src,'background-image');
+      hui.style.set(this.inner,{
         backgroundImage : bg
       });
       this.updateImage({node:this.inner,id:src.getAttribute('data-id'),width:hui.window.getViewWidth(),height:hui.window.getViewHeight()});
@@ -262,10 +262,10 @@
         //this.image = hui.build('img',{src:this.getUrl(src.getAttribute('data-id')),'class':'exhibit_viewer_image',style:{width:'500px',height:'500px'},parent:this.element})
       }
       src.style.opacity = '0';
-      _.cls.add(element,'is-small');
+      hui.cls.add(element,'is-small');
       window.setTimeout(function() {
-        _.cls.remove(element,'is-small');
-        _.cls.add(element,'is-full');
+        hui.cls.remove(element,'is-small');
+        hui.cls.add(element,'is-full');
         window.setTimeout(function() {
           element.style.transition = 'none';
           self.busy = false;
@@ -278,27 +278,27 @@
   var latestWide;
   var check = function() {
     var x = 1400 / 1227;
-    var w = _.window.getViewWidth();
-    var h = _.window.getViewHeight();
+    var w = hui.window.getViewWidth();
+    var h = hui.window.getViewHeight();
     var wide = w/h > 1;
     if (latestWide!==wide) {
-      _.onDraw(function() {
-        _.cls.set(document.body,'is-wide',wide);
+      hui.onDraw(function() {
+        hui.cls.set(document.body,'is-wide',wide);
       })
     }
     latestWide = wide;
   }
-  _.listen(window,'resize',check);
-  _.onReady(function() {
+  hui.listen(window,'resize',check);
+  hui.onReady(function() {
     check();
-    _.cls.add(_.find('body'),'is-layout')
+    hui.cls.add(hui.find('body'),'is-layout')
     exhibit.init();
   });
 
-  _.on(_.find('.exhibit_paintings'),'tap',function(ev) {
-    if (_.cls.has(ev.target,'exhibit_painting')) {
+  hui.on(hui.find('.exhibit_paintings'),'tap',function(ev) {
+    if (hui.cls.has(ev.target,'exhibit_painting')) {
       viewer.show({src:ev.target});
     }
   })
 
-})(hui)
+});
